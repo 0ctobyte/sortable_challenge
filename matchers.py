2760afcb9f499e9825cc4d96030aeb002dcb8e29
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2012-09-25 00:10:50 -0400

    Added code to match family string in listing['title'], if it exists Implemented find_str_in_list function to match a string in a list of strings

--- matchers.py
+++ matchers.py
@@ -4,18 +4,39 @@ import re
 MATCH_THRESHOLD = 0.90
 MATCH_THRESHOLD_FUZZY = 0.96
 
+def find_str_in_list(s, strings):
+	for index in range(0, len(strings)):
+		if strings[index] == s:
+			return index
+	else:
+		return -1
+
 def match(product, listing):
 	if listing['manufacturer'].lower().find(
 		product['manufacturer'].lower()) == -1:
 		return False
 
-	title = ''.join(re.split(r'\W+|_', listing['title'].lower()))
 	product_name = ''.join(re.split(r'\W+|_', product['product_name'].lower()))
 
-	if title.find(''.join(re.split(r'\W+|_', product['model'].lower()))) == -1:
+	title_no_syms = ''.join(re.split(r'\W+|_', listing['title'].lower()))
+	title_split = listing['title'].lower().split()
+	title_list_no_syms = [''.join(re.split(r'\W+|_', x)) for x in title_split]
+
+	model = ''.join(re.split(r'\W+|_', product['model'].lower()))
+	if find_str_in_list(model, title_list_no_syms) == -1:
 		return False
 
-	# Do the jaro-winkler! Sounds like a sweet dance move!
-	score = jaro_winkler(title[:len(product_name)], product_name)
+	divide = 2.0
+	score = 0.0
+	try:
+		family = ''.join(re.split(r'\W+|_', product['family'].lower()))
+		if find_str_in_list(family, title_list_no_syms) != -1:
+			score += 1.0
+	except KeyError:
+		divide = 1.0
+		score = 0.0
 
-	return True if score >= 0.92 else False
+	# Do the jaro-winkler! Sounds like a sweet dance move!
+	score += jaro_winkler(title_no_syms[:len(product_name)], product_name)
+
+	return True if score/divide >= 0.92 else False
